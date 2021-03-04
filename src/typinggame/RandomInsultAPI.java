63da8c537be06bb4876ad4121571e8fef96c6b55
MrPinko <fede02rosa@gmail.com>
2021-03-04 17:51:43 +0100

    dinomarrez (randomizer ma randomizzato)

--- src/typinggame/RandomInsultAPI.java
+++ src/typinggame/RandomInsultAPI.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -16,7 +17,7 @@ import java.util.stream.Collectors;
 public class RandomInsultAPI extends Thread {
 
     private static String nextInsult = "";
-    
+
     private static String temp = "";
 
     public static void initialize() {
@@ -35,37 +36,39 @@ public class RandomInsultAPI extends Thread {
     public RandomInsultAPI() {
         //thread constructor
     }
-    
+
     public void run() {
         //request insult
         //set 'nextInsult' to the response
-        
-        do{
+
+        do {
             temp = doRequest();
             //System.out.println("ciao - "+temp+"\nlmao - "+nextInsult);
             //System.out.println(!nextInsult.equals(temp));
-        }while(nextInsult.equals(temp));
-        
+        } while (nextInsult.equals(temp));
+
         nextInsult = temp;
-        
+
     }
-    
-    public String doRequest(){
+
+    private String urlApi = "https://evilinsult.com/generate_insult.php?lang=en";
+    private String urlDatabase = "http://93.56.110.73:3404/insults/random";
+
+    public String doRequest() {
         URL server;
         HttpURLConnection service;
         BufferedReader response;
-        
+
         int status;
         try {
-            String url = "https://evilinsult.com/generate_insult.php?lang=en"; // costruzione dello URL di interrogazione del servizio
             //System.out.println("URL: "+url);
-            server = new URL(url);
-            service = (HttpURLConnection)server.openConnection();
+            server = new URL(urlRandomizer());
+            service = (HttpURLConnection) server.openConnection();
             //service.setRequestProperty("Host", "127.0.0.1"); // impostazione header richiesta:  host
-            
+
             service.setRequestProperty("Accept", "text/plain"); // impostazione header richiesta:  formato risposta
             service.setRequestProperty("Accept-Charset", "UTF-8"); // impostazione header richiesta:  codifica risposta
-            
+
             service.setRequestMethod("GET"); // impostazione metodo di richiesta GET
             service.setDoInput(true); // attivazione ricezione
             service.connect(); // connessione al servizio
@@ -75,21 +78,30 @@ public class RandomInsultAPI extends Thread {
             }
             // apertura stream di ricezione da risorsa web
             response = new BufferedReader(new InputStreamReader(service.getInputStream(), "UTF-8"));
-            
+
             String s = response.lines().collect(Collectors.joining());
             response.close();
-            
+
             s = s.replaceAll("&quot;", "\"");
             s = s.replaceAll("&gt;", ">");
             s = s.replaceAll("&amp;", "&");
 
-
             return s;
-            
+
         } catch (IOException e) {
             e.printStackTrace();
         }
         return "";
+    }
+
+    private String urlRandomizer() {
+        Random r = new Random();
+
+        if (r.nextInt() % 2 == 0) {
+            return urlApi;
+        } else {
+            return urlDatabase;
+        }
     }
 
 }
